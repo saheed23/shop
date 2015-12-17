d9b8ce0c4c5831f3d6f621475111165a6b0aafcf
Saheed Salam <saheed.salam@gmail.com>
2015-12-17 15:38:14 -0600

    Adding basket code

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,8 +1,33 @@
 class CartController < ApplicationController
-  def index
-  end
-  def add
-	end
-	def clearCart
-	end
-	end
+def add
+   id = params[:id]
+      # if the cart has already been created, use the existing cart else create a new cart
+       if session[:cart] then
+          cart = session[:cart]
+       else
+           session[:cart] = {}
+           cart = session[:cart]
+      end
+    #If the product has already been added to the cart, increment the value else set it to zero
+     if cart[id] then
+        car[id] = cart[id] + 1
+     else
+        cart[id] = 1
+      end
+     redirect_to :action => :index
+end #end add method
+
+def clearCart
+     session[:cart] = nil
+     redirect_to :action => :index
+end
+
+def index
+    #if there is a cart, pass it to the page for display else an empty value
+    if session[:cart] then
+        @cart = session[:cart]
+   else
+       @cart = {}
+end
+end
+end
